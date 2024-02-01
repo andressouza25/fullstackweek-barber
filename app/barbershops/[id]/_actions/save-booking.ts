8d82daa86c55af6cb02c84ec02eb8999115d2462
Andre Santos <andressouza25@gmail.com>
2024-02-01 14:57:59 -0300

    feat: ensure a service can be booked by the user

---
+++ app/barbershops/[id]/_actions/save-booking.ts
@@ -0,0 +1,21 @@
+"use server"
+
+import { db } from "@/app/_lib/prisma";
+
+interface saveBookingParams{
+    barbershopId: string;
+    serviceId: string;
+    userId: string;
+    date: Date;
+}
+
+export const saveBooking =async (params:saveBookingParams) => {
+    await db.booking.create({
+        data:{
+            serviceId: params.serviceId,
+            userId: params.userId,
+            date: params.date,
+            barbershopId: params.barbershopId
+        }
+    })
+}
--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -13,11 +13,13 @@ import {
 } from "@/app/_components/ui/sheet";
 import { Barbershop, Service } from "@prisma/client";
 import { ptBR } from "date-fns/locale";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
-import { format } from "date-fns";
+import { format, setHours, setMinutes } from "date-fns";
+import { saveBooking } from "../_actions/save-booking";
+import { Loader2 } from "lucide-react";
 
 interface ServiceItemProps {
   barbershop: Barbershop;
@@ -25,9 +27,16 @@ interface ServiceItemProps {
   isAuthenticated: boolean;
 }
 
-const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps) => {
+const ServiceItem = ({
+  service,
+  barbershop,
+  isAuthenticated,
+}: ServiceItemProps) => {
+  const { data } = useSession();
+
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [hour, setHour] = useState<string | undefined>();
+  const [submitIsLoading, setSubmitIsLoading] = useState(false);
 
   const handleDateClick = (date: Date | undefined) => {
     setDate(date);
@@ -44,7 +53,32 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
     }
   };
 
-  // TODO: abrir modal de agendamento
+  const handleBookingSubmit = async () => {
+    setSubmitIsLoading(true);
+    try {
+      if (!hour || !date || !data?.user) {
+        return;
+      }
+
+      // Vai ajustar a hora pro DB: Exemplo 09:45
+      //["09", "45"]
+      const dateHour = Number(hour.split(":")[0]);
+      const dateMinutes = Number(hour.split(":")[1]);
+
+      const newDate = setMinutes(setHours(date, dateHour), dateMinutes);
+
+      await saveBooking({
+        serviceId: service.id,
+        barbershopId: barbershop.id,
+        date: newDate,
+        userId: (data.user as any).id,
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitIsLoading(false);
+    }
+  };
 
   const timeList = useMemo(() => {
     return date ? generateDayTimeList(date) : [];
@@ -151,28 +185,40 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                           {date && (
                             <div className="flex justify-between">
                               <h3 className="text-gray-400 text-sm">Data</h3>
-                              <h4 className="text-sm capitalize">{format(date, "dd  MMMM",{
-                                locale:ptBR,
-                              })}</h4>
+                              <h4 className="text-sm capitalize">
+                                {format(date, "dd  MMMM", {
+                                  locale: ptBR,
+                                })}
+                              </h4>
                             </div>
                           )}
-                          
+
                           {hour && (
                             <div className="flex justify-between">
                               <h3 className="text-gray-400 text-sm">Horário</h3>
                               <h4 className="text-sm capitalize">{hour}</h4>
                             </div>
                           )}
-                            <div className="flex justify-between">
-                              <h3 className="text-gray-400 text-sm">Barbearia</h3>
-                              <h4 className="text-sm capitalize">{barbershop.name}</h4>
-                            </div>
+                          <div className="flex justify-between">
+                            <h3 className="text-gray-400 text-sm">Barbearia</h3>
+                            <h4 className="text-sm capitalize">
+                              {barbershop.name}
+                            </h4>
+                          </div>
                         </CardContent>
                       </Card>
                     </div>
                     <SheetFooter className="px-5">
-                        <Button disabled={!hour || !date}>Confirmar reserva</Button>
-                      </SheetFooter>
+                      <Button
+                        onClick={handleBookingSubmit}
+                        disabled={!hour || !date || submitIsLoading}
+                      >
+                        {submitIsLoading && (
+                          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        )}
+                        Confirmar reserva
+                      </Button>
+                    </SheetFooter>
                   </SheetContent>
                 </Sheet>
               </div>
