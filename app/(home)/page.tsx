a8f8c2ec70d57d0876fafb529f16ee55f7554077
Andre Santos <andressouza25@gmail.com>
2024-02-02 18:28:57 -0300

    feat: add user name to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -35,7 +35,8 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, André!</h2>
+        
+        <h2 className="text-xl font-bold">{session?.user ? `Olá, ${session.user.name?.split('')[0]}`: 'Olá! Vamos agendar um corte hoje?'}</h2>
         <p className="capitalize">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
