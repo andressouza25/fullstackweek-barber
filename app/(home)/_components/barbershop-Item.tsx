d489ca45012a69e75833c47522776592379c72a7
Andre Santos <andressouza25@gmail.com>
2024-02-03 15:22:50 -0300

    feat: ensure user can search barbershop

--- app/(home)/_components/barbershop-Item.tsx
+++ app/(home)/_components/barbershop-Item.tsx
@@ -14,20 +14,21 @@ interface BarbershopItemProps {
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   const router = useRouter();
+
   const handleBookingClick = () => {
     router.push(`/barbershops/${barbershop.id}`);
-  }
+  };
 
   return (
-    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
-      <CardContent className="px-1 py-0">
+    <Card className="min-w-full max-w-full rounded-2xl">
+      <CardContent className="px-1 py-0 pt-1">
         <div className="w-full h-[159px] relative">
-            <div className="absolute top-2 left-2 z-50">
-            <Badge variant='secondary' className="opacity-90 flex gap-2 items-center top-3 left-3">
-                <StarIcon size={12} className="fill-primary text-primary" />
-                <span className="text-xs">5,0</span>
+          <div className="absolute top-2 left-2 z-50">
+            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
             </Badge>
-            </div>
+          </div>
           <Image
             alt={barbershop.name}
             src={barbershop.imageUrl}
@@ -38,13 +39,10 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
             className="rounded-2xl"
           />
         </div>
+
         <div className="px-2 pb-3">
-          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
-            {barbershop.name}
-          </h2>
-          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
-            {barbershop.address}
-          </p>
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
           <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
             Reservar
           </Button>
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,24 @@
 import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
-import BarbershopItem from "./_components/barbershop-Item";
+import BarbershopItem from "./_components/barbershop-item";
 import { getServerSession } from "next-auth";
-import { Key } from "react";
 import { authOptions } from "../_lib/auth";
+
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  const [barbershops, confirmedBookings] = await Promise.all([
+  const [barbershops, recommendedBarbershops, confirmedBookings] = await Promise.all([
     db.barbershop.findMany({}),
+    db.barbershop.findMany({
+      orderBy: {
+        id: "asc",
+      },
+    }),
     session?.user
       ? db.booking.findMany({
           where: {
@@ -35,9 +40,10 @@ export default async function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-        
-        <h2 className="text-xl font-bold">{session?.user ? `Olá, ${session.user.name?.split(' ')[0]}`: 'Olá! Vamos agendar um corte hoje?'}</h2>
-        <p className="capitalize">
+        <h2 className="text-xl font-bold">
+          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+        </h2>
+        <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })}
@@ -51,38 +57,36 @@ export default async function Home() {
       <div className="mt-6">
         {confirmedBookings.length > 0 && (
           <>
-            <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3 ">
-              Agendamento
-            </h2>
-            <div className="px-5 mt-6 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-              {confirmedBookings.map(
-                (booking: { id: Key | null | undefined }) => (
-                  <BookingItem key={booking.id} booking={booking} />
-                )
-              )}
+            <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+            <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+              {confirmedBookings.map((booking: { id: null | undefined; }) => (
+                <BookingItem key={booking.id} booking={booking} />
+              ))}
             </div>
           </>
         )}
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
-          Recomendados
-        </h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          {barbershops.map((barbershop: { id:  null | undefined; }) => (
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
           ))}
         </div>
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
-          Populares
-        </h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop: { id: Key | null | undefined }) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          {recommendedBarbershops.map((barbershop: { id:  null | undefined; }) => (
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
           ))}
         </div>
       </div>
